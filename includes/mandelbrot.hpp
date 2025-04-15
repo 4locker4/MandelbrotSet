c67a75fd9d0121002db92de73e90954a00aad092
4Locker4 <e.remchukov2006@gmail.com>
2025-04-15 11:57:57 +0300

    add arrays logic

--- includes/mandelbrot.hpp
+++ includes/mandelbrot.hpp
@@ -30,11 +30,28 @@ const int   Y_WINDOW_SIZE        = 800, X_WINDOW_SIZE        = 800;
 const int   Y_FPS_WINDOW         = 75,  X_FPS_WINDOW         = 100;
 const float FPS_FIELD_POSITION_X = 0.f, FPS_FIELD_POSITION_Y = 0.f;
 
-const float dx = 2.f / 800.f,     dy = 2.f / 600.f;
+const float dx = 10.f / 800.f,     dy = 10.f / 600.f;
+const float ds = 0.9;
 
 const float r2Max = 10.f;
 const int   nMax  = 256;
 
+const int   SIZE_OF_PIXEL_ARRAY  = 4;
+
 char FPS_STRING[9] = {};
+
+typedef struct
+{
+    float x = 0;
+    float y = 0;
+} PixelCoords;
+
+PixelCoords pixelArray [SIZE_OF_PIXEL_ARRAY] = {};
+
+void SetColor        (sf::Image * image, int x_coord, int y_coord, int * radiusN);
+int  CalculateSet    (sf::Image * image, MandelbrotData * MandelbrotSizees);
+int  StartRendering  ();
+
+
 
 #endif // MANDELBROT_HPP
--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,18 +1,31 @@
 #include "../includes/mandelbrot.hpp"
 #include "../includes/DSL.hpp"
+#include <mmintrin.h>
 
-int ClaculateSet (sf::Image * image, MandelbrotData * MandelbrotSizees);
+inline int CalculateRadius (float x, float y, float x0, float y0);
+
+#define GRAPHIC
 
 int main ()
 {
+    StartRendering ();
+
+    return 0;
+}
+
+int StartRendering ()
+{
     MandelbrotData  MandelbrotSizes  = {};                                              // Init shifts and scale
+
+    sf::Clock           clock;
+
+#ifdef GRAPHIC
 
     sf::RenderWindow    window;
     sf::Image           image;
     sf::RectangleShape  fpsField;
     sf::Font            fpsFont;
     sf::Text            fpsText;
-    sf::Clock           clock;
     sf::Texture         texture;
 
     window.create                (sf::VideoMode (X_WINDOW_SIZE, Y_WINDOW_SIZE), "Mandelbrot");
@@ -34,31 +47,43 @@ int main ()
     fpsText.setColor         (sf::Color::White);
     fpsText.setPosition      (FPS_FIELD_POSITION_X + 10, FPS_FIELD_POSITION_Y / 2 + 20);
 
+#endif // GRAPHIC
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //                                  Start
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    while (window.isOpen ())
-    {        
+    while (true)
+    {
+
+#ifdef GRAPHIC
+
         image.create (X_WINDOW_SIZE, Y_WINDOW_SIZE, sf::Color::White);
 
 //======   Shifts   ====================================================================
-        if (MoveLeft)   MandelbrotSizes.shift_x -= dx;
-        if (MoveRight)  MandelbrotSizes.shift_x += dx;
-        if (MoveDown)   MandelbrotSizes.shift_y += dy;
-        if (MoveUp)     MandelbrotSizes.shift_y -= dy;
-        if (ScalePlus)  MandelbrotSizes.scale   += dx;
-        if (ScaleMinus) MandelbrotSizes.scale   -= dx;
+        if (MoveLeft)   MandelbrotSizes.shift_x -= dx / MandelbrotSizes.scale;
+        if (MoveRight)  MandelbrotSizes.shift_x += dx / MandelbrotSizes.scale;
+        if (MoveDown)   MandelbrotSizes.shift_y += dy / MandelbrotSizes.scale;
+        if (MoveUp)     MandelbrotSizes.shift_y -= dy / MandelbrotSizes.scale;
+        if (ScalePlus)  MandelbrotSizes.scale   *= ds;
+        if (ScaleMinus) MandelbrotSizes.scale   /= ds;
 
         if (ExitWindow) break;                                  // Break
 
         CheckToSaveCoords                                       // Check if user wants to save shifts
 //======   ------   ====================================================================
 
+#endif // GRAPHIC
+
+#ifndef GRAPHIC
+
+        sf::Image image;
+
+#endif // GRAPHIC
+
         clock.restart ();                      // Start FPS
         
-        ClaculateSet (&image, &MandelbrotSizes);
+        CalculateSet (&image, &MandelbrotSizes);
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //                                  Take FPS
@@ -66,6 +91,14 @@ int main ()
 
         float passed_time = clock.getElapsedTime().asSeconds();
         float FPS = 1 / passed_time;
+
+#ifndef GRAPHIC
+
+        printf ("FPS:   %2.f\n", FPS);
+
+#endif // GRAPHIC
+
+#ifdef GRAPHIC
 
         snprintf (FPS_STRING, 9, "FPS: %.f", FPS);
         fpsText.setString (FPS_STRING);
@@ -80,44 +113,78 @@ int main ()
         window.draw     (fpsField);
         window.draw     (fpsText);
         window.display  ();
+
+#endif // GRAPHIC
     }
 
     return 0;
 }
 
-int ClaculateSet (sf::Image * image, MandelbrotData * MandelbrotSizes)
+int CalculateSet (sf::Image * image, MandelbrotData * MandelbrotSizes)
 {
     for (int y_coord = 0; y_coord < Y_WINDOW_SIZE; y_coord++)
     {
         float x0 = ((                - 400.f) * dx + MandelbrotSizes->shift_x - 1.f) * MandelbrotSizes->scale,
               y0 = (((float) y_coord - 400.f) * dy + MandelbrotSizes->shift_y + 0.f) * MandelbrotSizes->scale;
 
-        for (int x_coord = 0; x_coord < X_WINDOW_SIZE; x_coord++, x0 += dx)
+        for (int x_coord = 0; x_coord < X_WINDOW_SIZE; x_coord += SIZE_OF_PIXEL_ARRAY, x0 += 4 * dx)
         {
-            float x = x0,
-                  y = y0;
+            float x_base  [SIZE_OF_PIXEL_ARRAY] = {x0         ,
+                                                   x0 + dx    ,
+                                                   x0 + dx * 2,
+                                                   x0 + dx * 3};
 
-            int N = 0;
-
-            for ( ; N < nMax; N++)
+            int   radiosN [SIZE_OF_PIXEL_ARRAY] = {0};
+            
+            for (int i = 0; i < SIZE_OF_PIXEL_ARRAY; i++)
             {
-                float x2 = x * x,
-                      y2 = y * y,
-                      xy = x * y;
-
-                float r2 = x2 + y2;
-
-                if (r2 >= r2Max) break;
-
-                x = x2 - y2 + x0;
-                y = 2 * xy + y0;
+                radiosN[i] = CalculateRadius (x_base[i], y0, x0, y0);
             }
 
-            sf::Color color = (N == nMax) ? sf::Color::Black : sf::Color ((uint8_t) sqrt (255 - N * 0.8), (uint8_t) sqrt (N * 3), (uint8_t) 255 - 255 % N);
+#ifdef GRAPHIC
 
-            image->setPixel (x_coord, y_coord, color);
+            SetColor (image, x_coord, y_coord, radiosN);
+
+#endif // GRAPHIC
         }
     }
 
     return 0;
+}
+
+void SetColor (sf::Image * image, int x_coord, int y_coord, int * radiusN)
+{
+    sf::Color color;
+
+    for (int i = 0; i < SIZE_OF_PIXEL_ARRAY; i++)
+    {
+        color = (radiusN[i] == nMax) ? sf::Color::Black : sf::Color ((uint8_t) radiusN[i] * 18 % 255, 
+                                                                     (uint8_t) radiusN[i] * 10 % 255, 
+                                                                     (uint8_t) radiusN[i] * 15 % 255);
+
+        image->setPixel (x_coord + i, y_coord, color);
+    }
+
+    return;
+}
+
+inline int CalculateRadius (float x, float y, float x0, float y0)
+{
+    int N = 0;
+
+    for ( ; N < nMax; N++)
+    {
+        float x2 = x * x,
+              y2 = y * y,
+              xy = x * y;
+
+        float r2 = x2 + y2;
+
+        if (r2 >= r2Max) return N;
+
+        x = x2 - y2 + x0;
+        y = 2 * xy + y0;
+    }
+
+    return N;
 }
